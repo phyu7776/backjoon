2f4b82fc0c9eed5b92506a35e4ce51b1e4555c33
phyu7776 <58426506+phyu7776@users.noreply.github.com>
2023-05-12 22:35:47 +0900

    [Bronze II] Title: Hashing, Time: 208 ms, Memory: 17768 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -4,7 +4,7 @@ class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long r = 31;
+        long r = 1;
         long m = 1234567891;
 
         int l = sc.nextInt();
@@ -14,8 +14,8 @@ class Main {
         long result = 0;
         for(int i = 0 ; i < alpha.length() ; i++){
             int x = (int)alpha.charAt(i)-96;
-
-            result += ( (x%m) * (Math.pow(r,i)%m) ) % m;
+            result += (x*r)%m;
+            r = (r*31)%m;
         }
 
         System.out.println(result);
