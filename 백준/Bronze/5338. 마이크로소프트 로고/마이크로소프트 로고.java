c140f600f0798360d3d9ce7abbd17b8bcaa95e8a
phyu7776 <58426506+phyu7776@users.noreply.github.com>
2023-06-21 22:47:38 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 112 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,13 @@
+
+class Main{
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("       _.-;;-._").append("\n");
+        sb.append("'-..-'|   ||   |").append("\n");
+        sb.append("'-..-'|_.-;;-._|").append("\n");
+        sb.append("'-..-'|   ||   |").append("\n");
+        sb.append("'-..-'|_.-''-._|").append("\n");
+
+        System.out.print(sb);
+    }
+}
