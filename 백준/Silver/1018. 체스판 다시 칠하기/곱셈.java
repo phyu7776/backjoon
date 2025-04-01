e2d4d3a65e45e342f85ae23923fc62e564905b69
Hambbong <phyu7776@gmail.com>
2025-04-01 23:15:48 +0900

    1

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/곱셈.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+
+        BigInteger a = new BigInteger(input[0]);
+        BigInteger b = new BigInteger(input[1]);
+        BigInteger c = new BigInteger(input[2]);
+
+        System.out.print(a.modPow(b,c));
+        
+        뭐임 커밋 왜안댐
+    }
+}
